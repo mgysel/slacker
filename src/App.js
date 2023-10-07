bfe1c95be62c200f1fdce73ea3c090f98c83bc82
Mike Gysel <mikegysel@gmail.com>
2023-10-07 20:07:29 +0200

    Updated frontend_master to just contain frontend files. Allows for heroku serve

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import HomePage from './pages/HomePage';
-import ChannelPage from './pages/ChannelPage';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import ResetPasswordPage from './pages/ResetPasswordPage';
-import ProfilePage from './pages/ProfilePage';
-import SearchPage from './pages/SearchPage';
+import HomePage from "./pages/HomePage";
+import ChannelPage from "./pages/ChannelPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import ProfilePage from "./pages/ProfilePage";
+import SearchPage from "./pages/SearchPage";
 
-import ProtectedRoute from './components/Layout/ProtectedRoute';
+import ProtectedRoute from "./components/Layout/ProtectedRoute";
 
-import { AuthProvider } from './AuthContext';
-import './axios';
+import { AuthProvider } from "./AuthContext";
+import "./axios";
 
 function App() {
   const [authDetails, setAuthDetails] = React.useState(
-    localStorage.getItem('token')
+    localStorage.getItem("token")
   );
 
   function setAuth(token, u_id) {
-    localStorage.setItem('token', token);
-    localStorage.setItem('u_id', u_id);
+    localStorage.setItem("token", token);
+    localStorage.setItem("u_id", u_id);
     setAuthDetails(token);
   }
   return (
@@ -34,14 +34,14 @@ function App() {
           <Route
             exact
             path="/login"
-            render={(props) => {
+            render={props => {
               return <LoginPage {...props} setAuth={setAuth} />;
             }}
           />
           <Route
             exact
             path="/register"
-            render={(props) => {
+            render={props => {
               return <RegisterPage {...props} setAuth={setAuth} />;
             }}
           />
